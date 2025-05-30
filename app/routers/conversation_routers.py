24556e27eeebc417b3ac495e21103324f506b69a
t-dlsantos <thiagodlsantos9@gmail.com>
2025-05-30 20:50:03 -0300

    feat: enhance chat creation by adding theme_title to request body and set default message

--- app/routers/conversation_routers.py
+++ app/routers/conversation_routers.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from uuid import uuid4
 from app.utils.di_container import get_chat_service
 from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from sse_starlette.sse import EventSourceResponse
 
 from app.services.db import get_redis, chat_exists
@@ -12,19 +12,22 @@ from app.assistants import INITIAL_PROMPT
 
 
 class ChatIn(BaseModel):
-    message: str
+    message: str = Field(default=INITIAL_PROMPT)
+
+class NewChatIn(BaseModel):
+    theme_title: str = Field(default='Coffee') # todo :: Ver se essa é a melhor forma de receber o tema do chat
 
 router = APIRouter()
 
 @router.post('/chats')
-async def new_chat(chat_service: ChatService = Depends(get_chat_service)):
+async def new_chat(chat_in: NewChatIn, chat_service: ChatService = Depends(get_chat_service)):
     # todo :: Receber theme_title pelo body do post
-    chat = await chat_service.create_chat()
+    chat = await chat_service.create_chat(chat_in.theme_title)
     # todo :: Validar se precisa mesmo retornar objeto
-    return {'id': chat.id}
+    return {'id': chat['id']}
 
 @router.post('/chats/{chat_id}')
-async def chat(chat_id: str, chat_in = ChatIn(message=INITIAL_PROMPT)):
+async def chat(chat_id: str, chat_in: ChatIn):
     rdb = get_redis()
     if not await chat_exists(rdb, chat_id):
         raise HTTPException(status_code=404, detail=f'Chat {chat_id} does not exist')
