31cf8a2ec09e852b2d0b83f137bed38e35823940
t-dlsantos <thiagodlsantos9@gmail.com>
2025-06-17 18:20:09 -0300

    refactor: remove deprecated chat endpoint from conversation routers

--- app/routers/conversation_routers.py
+++ app/routers/conversation_routers.py
@@ -30,20 +30,6 @@ async def new_chat(chat_in: NewChatIn, chat_service: ChatService = Depends(get_c
     # todo :: Validar se precisa mesmo retornar objeto
     return {'id': chat['id']}
 
-@router.post('/chats/{chat_id}')
-async def chat(chat_id: str, chat_in: ChatIn, chat_service: ChatService = Depends(get_chat_service)):
-    rdb = get_redis()
-    if not await chat_exists(rdb, chat_id):
-        raise HTTPException(status_code=404, detail=f'Chat {chat_id} does not exist')
-    
-    assistant = RAGAssistant(
-        chat_id=chat_id,
-        rdb=rdb,
-        chat_service=chat_service
-    )
-    sse_stream = assistant.run(message=chat_in.message)
-    return EventSourceResponse(sse_stream, background=rdb.aclose)
-
 @router.websocket("/ws/solo/{chat_id}/{client_id}")
 async def websocket_solo_practice(websocket: WebSocket, chat_id: str, client_id: str, chat_service: ChatService = Depends(get_chat_service)):
     await websocket.accept()
